d4085595f0288c3829ae6b4ad64a78c6310701ca
vsoch <vsochat@stanford.edu>
2016-08-12 14:59:44 -0700

    adding pubmed search / articles to visualization to close #6

--- cegid/apps/main/static/js/main.js
+++ cegid/apps/main/static/js/main.js
@@ -1,13 +1,3 @@
-/*price range*/
-
- $('#sl2').slider();
-
-	var RGBChange = function() {
-	  $('#RGB').css('background', 'rgb('+r.getValue()+','+g.getValue()+','+b.getValue()+')')
-	};	
-		
-/*scroll to top*/
-
 $(document).ready(function(){
 	$(function () {
 		$.scrollUp({
